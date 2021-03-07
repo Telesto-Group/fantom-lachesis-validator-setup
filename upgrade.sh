fe85a7f82c3a7f5005fddf0220a5de83b36bf205
mhetzel <34289010+mhetzel@users.noreply.github.com>
2021-03-07 10:38:26 -0600

    Update and rename testargs.sh to upgrade.sh

---
+++ upgrade.sh
@@ -0,0 +1,29 @@
+if [ $# -eq 1 ]; then
+    LACHESIS_VERSION="$1"
+else
+    echo "Please pass in a lachesis version"
+    exit
+fi
+
+# Check lachesis installtion and install if needed
+INSTALLED_LACHESIS=$(lachesis version)
+if [[ ! -z "$INSTALLED_LACHESIS" && "$INSTALLED_LACHESIS" =~ .*"$LACHESIS_VERSION".* ]]
+then
+  echo "lachesis is already installed"
+else
+  echo "Updating lachesis from $INSTALLED_LACHESIS to $LACHESIS_VERSION"
+  # Install Lachesis
+  cd $LACHESISPATH
+  git checkout tags/v$LACHESIS_VERSION
+  make build
+  cd ~
+fi
+# Validate lachesis version before moving on
+INSTALLED_LACHESIS=$(lachesis version)
+echo $INSTALLED_LACHESIS
+if [[ ! -z "$INSTALLED_LACHESIS" && "$INSTALLED_LACHESIS" =~ .*"$LACHESIS_VERSION".* ]]
+then
+  echo "lachesis updated to $LACHESIS_VERSION.  Go ahead and start your node"
+else
+  echo "lachesis did not update correctly for some reason. You're in trouble."
+fi
