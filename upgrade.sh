c46636ee7bd4223fd6ff57b349f2b25eaf96e12f
mhetzel <34289010+mhetzel@users.noreply.github.com>
2021-03-07 13:28:52 -0600

    Update version (#9)

---
+++ upgrade.sh
@@ -0,0 +1,35 @@
+if [ $# -eq 1 ]; then
+    NEW_LACHESIS_VERSION="$1"
+else
+    echo "Please pass in a lachesis version"
+    exit
+fi
+
+sed -i "s,$LACHESIS_VERSION,$NEW_LACHESIS_VERSION," $HOME/.zshrc
+sed -i "s,$LACHESIS_VERSION,$NEW_LACHESIS_VERSION," $HOME/.bash_profile
+sed -i "s,$HOME/go/Fantom-foundations/go-lachesis/,$LACHESISPATH/," $HOME/.bash_profile
+
+# Check lachesis installtion and install if needed
+INSTALLED_LACHESIS=$(lachesis version)
+if [[ ! -z "$INSTALLED_LACHESIS" && "$INSTALLED_LACHESIS" =~ .*"$NEW_LACHESIS_VERSION".* ]]
+then
+  echo "lachesis is already running desired version"
+  exit
+else
+  echo "Updating lachesis from $LACHESIS_VERSION to $NEW_LACHESIS_VERSION"
+  # Install Lachesis
+  cd $LACHESISPATH
+  git checkout tags/v$NEW_LACHESIS_VERSION
+  make build
+  cd ~
+fi
+
+# Validate lachesis version before moving on
+INSTALLED_LACHESIS=$(lachesis version)
+echo $INSTALLED_LACHESIS
+if [[ ! -z "$INSTALLED_LACHESIS" && "$INSTALLED_LACHESIS" =~ .*"$NEW_LACHESIS_VERSION".* ]]
+then
+  echo "lachesis updated to $NEW_LACHESIS_VERSION.  Go ahead and start your node"
+else
+  echo "lachesis did not update correctly for some reason. You're in trouble."
+fi
--- validatorSetup.sh
+++ validatorSetup.sh
@@ -8,13 +8,13 @@
 # ./validatorSetup.sh myLachesisUser
 # ./validatorSetup.sh myLachesisUser 0.7.0-rc.1
 
-if [ $# -ne 1 ]; then
-    echo "Please pass in a username to create"
+if [ $# -lt 1 ]; then
+    echo "Please pass in at least a username to create"
     exit
 fi
 
-if [ $# -eq $2 ]; then
-    LACHESIS_VERSION=$2
+if [ $# -eq 2 ]; then
+    LACHESIS_VERSION="$2"
 else
     LACHESIS_VERSION='0.7.0-rc.1'
 fi
@@ -33,6 +33,7 @@ if id "$USER" &>/dev/null; then
   echo "$USER user already exists"
   sudo rm -rf /home/$USER
 else
+  echo "creating user: $USER"
   sudo useradd -m $USER
 fi
 
@@ -45,7 +46,11 @@ sudo usermod -aG sudo $USER
 sudo chown -R $USER:$USER /home/$USER/
 sudo chmod 700 /home/$USER/.ssh
 sudo chmod 644 /home/$USER/.ssh/authorized_keys
-echo "$USER ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/dont-prompt-$USER-for-password
+
+FILE=/etc/sudoers.d/dont-prompt-$USER-for-password
+if ! test -f "$FILE"; then
+  echo "$USER ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee $FILE
+fi
 
 # Setup zsh for interaction later
 sudo usermod --shell /bin/zsh $USER
@@ -55,7 +60,7 @@ sudo cp /home/$USER/.oh-my-zsh/templates/zshrc.zsh-template /home/$USER/.zshrc
 # Define Paths
 GOROOT=/usr/local/go
 GOPATH=/home/$USER/go
-LACHESISPATH=$GOPATH/src/github.com/Fantom-foundation
+LACHESISPATH=$GOPATH/src/github.com/Fantom-foundation/go-lachesis
 LACHESISURL=https://github.com/Fantom-foundation/go-lachesis.git
 PATHS=$GOPATH/go/bin:$GOROOT/bin:$LACHESISPATH/build
 ZSHRC=/home/$USER/.zshrc
@@ -68,7 +73,6 @@ echo "export GO_VERSION=$GO_VERSION" | sudo tee -a $BASHPROFILE
 echo "export LACHESISPATH=$LACHESISPATH" | sudo tee -a $BASHPROFILE
 echo "export LACHESISURL=$LACHESISURL" | sudo tee -a $BASHPROFILE
 echo "export LACHESIS_VERSION=$LACHESIS_VERSION" | sudo tee -a $BASHPROFILE
-echo "export LACHESIS_BRANCH=$LACHESIS_BRANCH" | sudo tee -a $BASHPROFILE
 echo "export PATH=$PATH:$PATHS" | sudo tee -a $BASHPROFILE
 
 # Setup zsh environment for user
@@ -78,7 +82,6 @@ echo "export GO_VERSION=$GO_VERSION" | sudo tee -a $ZSHRC
 echo "export LACHESISPATH=$LACHESISPATH" | sudo tee -a $ZSHRC
 echo "export LACHESISURL=$LACHESISURL" | sudo tee -a $ZSHRC
 echo "export LACHESIS_VERSION=$LACHESIS_VERSION" | sudo tee -a $ZSHRC
-echo "export LACHESIS_BRANCH=$LACHESIS_BRANCH" | sudo tee -a $ZSHRC
 echo "export PATH=$PATH:$PATHS" | sudo tee -a $ZSHRC
 
 # Switch to new user to continue setup
@@ -92,7 +95,7 @@ sudo su - $USER --shell /bin/bash <<- 'EOT'
     GO_FILE=$GO_VERSION.linux-amd64.tar.gz
     wget https://dl.google.com/go/$GO_FILE
     sudo rm -rf $GOROOT
-    sudo tar -xvf $GO_FILE -C /usr/local
+    sudo tar -xf $GO_FILE -C /usr/local
   fi
   # Validate go version before moving on
   INSTALLED_GO=$(go version)
